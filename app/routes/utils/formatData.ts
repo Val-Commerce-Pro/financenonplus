59ebf93e31d78b35e87c00844e14f01c2d43afb0
valdenicio22 <valdenicio22@gmail.com>
2024-05-14 11:29:21 +0200

    fix: formData to action func

---
+++ app/routes/utils/formatData.ts
@@ -0,0 +1,30 @@
+type FormatData = {
+  [k: string]: FormDataEntryValue;
+};
+
+export const formatData = (
+  values: FormatData,
+  hasBoolean: boolean = false,
+): {
+  [key: string]: string | boolean;
+} => {
+  const booleanKeys = ["appMode"];
+  const castedValues = Object.entries(values).reduce(
+    (acc, [key, value]) => {
+      if (hasBoolean && booleanKeys.includes(key)) {
+        acc[key] = value.toString() === "true";
+      } else {
+        acc[key] = value.toString();
+      }
+      return acc;
+    },
+    {} as { [key: string]: string | boolean },
+  );
+  return castedValues;
+};
+
+export type OptionsMethodData = Array<{
+  id: string;
+  labelValue: string;
+  selected?: boolean;
+}>;
