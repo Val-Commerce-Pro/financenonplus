2551273a9c36564a93680c681d73258a54da7790
Dejan Simic <ds@commerce-pro.de>
2024-05-28 09:50:24 +0200

    Add real numbers and first styling

--- extensions/finance-extension/assets/consors-product.js
+++ extensions/finance-extension/assets/consors-product.js
@@ -26,11 +26,85 @@ async function addProductToCart() {
   return data;
 }
 
+async function getPluginConfData() {
+  // const shop = document.getElementById("shopDomain")?.textContent;
+  const shop = "financenonplus.myshopify.com";
+  try {
+    const parameters = new URLSearchParams({ shop });
+    const requestUrl = `https://financenonplus.cpro-server.de/api/getPluginConfData?${parameters}`;
+
+    const response = await fetch(requestUrl, { method: "GET" });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching AppConfig:", error);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
+  const pluginConfData = await getPluginConfData();
   // const secureUrl = document.getElementById("cf-secure-url").textContent;
   const addProductAndRedirect = document.getElementById(
     "addProductAndRedirect",
   );
+  const calculatorInitiator = document.getElementById(
+    "additional-calculator-text",
+  );
+  if (!pluginConfData.pluginConfigurator.appMode) {
+    calculatorInitiator.classList.add("HiddenInfo");
+  }
+
+  // Init calculator
+  jQuery(function ($) {
+    var productPrice = document.getElementById("cf-product-price").textContent;
+
+    let minMonth = parseInt(
+      pluginConfData.pluginConfigurator.terms.split(",")[0],
+    );
+    let maxMonth = parseInt(
+      pluginConfData.pluginConfigurator.terms.split(",")[1],
+    );
+    let stepMonth = parseInt(
+      pluginConfData.pluginConfigurator.terms.split(",")[2],
+    );
+    let firstInterestRate = parseFloat(
+      pluginConfData.pluginConfigurator.interestRate.split(",")[0],
+    );
+    let secondInterestRate = parseFloat(
+      pluginConfData.pluginConfigurator.interestRate.split(",")[1],
+    );
+    let thirdInterestRate = parseFloat(
+      pluginConfData.pluginConfigurator.interestRate.split(",")[2],
+    );
+    let zeroMonth = pluginConfData.pluginConfigurator.campaignDuration;
+
+    // switch (pluginConfData.pluginConfigurator.campaign) {
+    //   case 1:
+    //     break;
+    //   case 2:
+    //     //default case
+    //     break;
+    //   case 3:
+    //     minMonth = maxMonth = zeroMonth;
+    //     stepMonth = 0;
+    //     firstInterestRate = secondInterestRate = thirdInterestRate = 0;
+    //     break;
+    // }
+
+    $("#calculator").calculator({
+      minMonth: minMonth,
+      maxMonth: maxMonth,
+      stepMonth: stepMonth,
+      zeroMonth: 24 + 1,
+      firstInterestRate: firstInterestRate,
+      secondInterestRate: secondInterestRate,
+      thirdInterestRate: thirdInterestRate,
+      productPrice: 1300,
+    });
+  });
 
   addProductAndRedirect.addEventListener("click", async (e) => {
     await addProductToCart();
