d50fed3cad3edc2f5ed611a0b2d246cd47610672
Dejan Simic <ds@commerce-pro.de>
2024-05-28 13:32:16 +0200

    Dynamically render consors

--- extensions/finance-extension/assets/consors-product.js
+++ extensions/finance-extension/assets/consors-product.js
@@ -1,3 +1,12 @@
+const observer = new MutationObserver((list) => {
+  const evt = new CustomEvent("dom-changed", { detail: list });
+  document.body.dispatchEvent(evt);
+});
+observer.observe(document.body, {
+  attributes: true,
+  childList: true,
+});
+
 async function addProductToCart() {
   const productId = document.getElementById("cf-product-id").textContent;
   const secureUrl = document.getElementById("cf-secure-url").textContent;
@@ -46,7 +55,7 @@ async function getPluginConfData() {
 
 document.addEventListener("DOMContentLoaded", async () => {
   const pluginConfData = await getPluginConfData();
-  // const secureUrl = document.getElementById("cf-secure-url").textContent;
+  const secureUrl = document.getElementById("cf-secure-url").textContent;
   const addProductAndRedirect = document.getElementById(
     "addProductAndRedirect",
   );
@@ -56,6 +65,36 @@ document.addEventListener("DOMContentLoaded", async () => {
   if (!pluginConfData.pluginConfigurator.appMode) {
     calculatorInitiator.classList.add("HiddenInfo");
   }
+
+  const productPrice = document.getElementById("cf-product-price").textContent;
+
+  const cartPrice = document.getElementById("cf-cart-price").textContent;
+
+  if (
+    (parseInt(cartPrice) + parseInt(productPrice)) / 100 <
+    pluginConfData.pluginConfigurator.minOrderValue
+  ) {
+    document.getElementById("cf-product-section").classList.add("HiddenInfo");
+  }
+
+  document.body.addEventListener("dom-changed", async (e) => {
+    const cartPrice = await fetch(secureUrl + "/cart.js")
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error) => console.error("Error fetching cart data:", error));
+
+    if (
+      parseInt(cartPrice.total_price) + parseInt(productPrice) / 100 <
+      pluginConfData.pluginConfigurator.minOrderValue
+    ) {
+      document.getElementById("cf-product-section").classList.add("HiddenInfo");
+    } else {
+      document
+        .getElementById("cf-product-section")
+        .classList.remove("HiddenInfo");
+    }
+  });
 
   // Init calculator
   jQuery(function ($) {
@@ -80,9 +119,14 @@ document.addEventListener("DOMContentLoaded", async () => {
     let thirdInterestRate = parseFloat(
       pluginConfData.pluginConfigurator.interestRate.split(",")[2],
     );
-    let zeroMonth = pluginConfData.pluginConfigurator.campaignDuration;
+    let zeroMonth = parseInt(
+      pluginConfData.pluginConfigurator.campaignDuration,
+    );
 
     // switch (pluginConfData.pluginConfigurator.campaign) {
+    //   case 0:
+    //     zeroMonth = 0;
+    //     break;
     //   case 1:
     //     break;
     //   case 2:
@@ -95,11 +139,22 @@ document.addEventListener("DOMContentLoaded", async () => {
     //     break;
     // }
 
+    console.log("DATA", {
+      minMonth: minMonth,
+      maxMonth: maxMonth,
+      stepMonth: stepMonth,
+      zeroMonth: zeroMonth + 1, // Increment so that passed month is inclusive
+      firstInterestRate: firstInterestRate,
+      secondInterestRate: secondInterestRate,
+      thirdInterestRate: thirdInterestRate,
+      productPrice: 1300, //test data because real store values are too small
+    });
+
     $("#calculator").calculator({
       minMonth: minMonth,
       maxMonth: maxMonth,
       stepMonth: stepMonth,
-      zeroMonth: 24 + 1, // Increment so that passed month is inclusive
+      zeroMonth: zeroMonth + 1, // Increment so that passed month is inclusive
       firstInterestRate: firstInterestRate,
       secondInterestRate: secondInterestRate,
       thirdInterestRate: thirdInterestRate,
