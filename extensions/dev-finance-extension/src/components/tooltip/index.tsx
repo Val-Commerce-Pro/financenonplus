14c73de7e12cbf084b7b07196835dc563931cbfe
Dejan Simic <ds@commerce-pro.de>
2024-05-29 12:00:36 +0200

    Show tooltip in shopify app

--- extensions/dev-finance-extension/src/components/tooltip/index.tsx
+++ extensions/dev-finance-extension/src/components/tooltip/index.tsx
@@ -4,8 +4,13 @@ interface TooltipProps {
 
 export const Tooltip = ({ text }: TooltipProps) => {
   return (
-    <div className="absolute bottom-full mb-[8px] hidden w-max max-w-[320px] p-[12px] text-[14px] text-left text-white bg-gray-900 rounded-[8px] shadow-md transition-opacity duration-200 group-hover:block dark:bg-gray-700">
-      {text}
-    </div>
+    <>
+      <div
+        style={{ visibility: "hidden" }}
+        className="absolute bottom-full mb-[8px] w-max max-w-[320px] p-[12px] text-[14px] text-left text-white bg-gray-900 rounded-[8px] shadow-md transition-opacity duration-200 group-hover:!visible dark:bg-gray-700"
+      >
+        {text}
+      </div>
+    </>
   );
 };
